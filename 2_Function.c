96cef810df5e598e594c63d4c73e5ea34d19d2ce
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-07-09 23:50:55 +0530

    Function in c

---
+++ 2_Function.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<math.h>
+//Declaration
+void SachiDev();
+void Bye();
+
+
+
+
+//Function call
+int main(){
+    SachiDev();
+    Bye();
+    SachiDev();
+    Bye();
+    SachiDev();
+    Bye();
+    SachiDev();
+    Bye();
+    SachiDev();
+    Bye();
+    SachiDev();
+    Bye();
+    SachiDev();
+    Bye();
+    SachiDev();
+    Bye();
+    SachiDev();
+    Bye();
+    SachiDev();
+    Bye();
+    SachiDev();
+    Bye();
+
+
+
+    return 0;
+
+}
+
+
+
+//Definition
+void SachiDev(){
+    printf("_SachiDev_\n");
+}
+
+void Bye(){
+    printf("Good Bye\n");
+}
+
+/* OUTPUT-- _SachiDev_
+            Good Bye 
+            11 Times */
