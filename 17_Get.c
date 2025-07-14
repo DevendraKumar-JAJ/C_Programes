855f5581455ca2aafe3e04fd51237bf6722b9e43
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-07-14 11:52:06 +0530

    getch getchar getche function in c

---
+++ 17_Get.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+void main(){
+    char a,b,c;
+
+    printf("Enter later\n");
+    a=getch();
+    printf("%c\n",a);
+
+    printf("Enter later\n");
+    b=getchar();
+    printf("%c\n",b);
+
+    printf("Enter later\n");
+    c=getche();
+    printf("%c\n",c);
+}
