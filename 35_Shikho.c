82808ddce9fd48218b83aadfc64bfce81437c230
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-07-16 08:53:14 +0530

    Typecasting 2

---
+++ 35_Shikho.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    //type casting...
+    int d=5,f,j;
+    float g=1.5,a,b;
+    char q='b',y,u;
+    y=(char)g;
+    u=(char)d;
+    a=(float)q;
+    b=(float)d;
+    f=(int)g;
+    j=(int)q;
+
+    printf("\n%c",y);
+    printf("\n%c",u);
+    printf("\n%f",a);
+    printf("\n%f",b);
+    printf("\n%d",f);
+    printf("\n%d",j);
+
+
+}
