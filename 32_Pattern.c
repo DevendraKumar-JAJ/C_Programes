8d3fed8a10948ad7c47a8f1bf8ef3f08921a884b
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-07-16 08:51:34 +0530

    Pattern in c

---
+++ 32_Pattern.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    for (int i = 1; i <=9; i++)
+    {
+        if (i<=5)
+            {
+                    for ( int n= 1; n <=9; n++)
+                    {
+                        if (n<=5)
+                        {
+                            if ()
+                            {
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                        printf("\n");
+                
+            }
+            else   
+            {
+            for ( int l = 1; l<=5; l++)
+            {
+                if ((l+i)%2==0&&(i-l)<=4)
+                {
+                    printf("0");
+                }
+                else
+                {
+                    printf(" ");
+                }
+        
+            }
+            printf("\n");
+    
+        }
+    }
+
+    getch();
+}
