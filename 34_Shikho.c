a9f16712984b722277fc2646083ca30dcae00389
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-07-16 08:52:40 +0530

    Sizeof

---
+++ 34_Shikho.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int x;
+    printf("\n%d",sizeof(x));
+    printf("\n%d",sizeof(int));
+
+    printf("\n%d",sizeof(5));
+
+    printf("\n%d",sizeof(1.00));
+
+    printf("\n%d",sizeof(float));
+
+    printf("\n%d",sizeof(double));
+
+    printf("\n%d",sizeof(long double));
+        printf("\n%d",sizeof('C'));
+
+    printf("\n%d",sizeof(char));
+
+
+}
